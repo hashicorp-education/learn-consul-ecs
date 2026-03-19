ac6b3f365441280d7b714f1f661df6919035dd4d
Krastin Krastev <krastin@hashicorp.com>
2026-03-19 11:56:23 +0100

    bumping consul version up

--- variables.tf
+++ variables.tf
@@ -21,13 +21,13 @@ variable "vpc_region" {
 variable "consul_version" {
   type        = string
   description = "The Consul version"
-  default     = "1.17.0"
+  default     = "1.21.5"
 }
 
 variable "chart_version" {
   type        = string
   description = "The Consul Helm chart version to use"
-  default     = "1.3.0"
+  default     = "1.8.10"
 }
 
 variable "datacenter" {
